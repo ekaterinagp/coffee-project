c069902090a4be8e36309420aba51263084fef5f
Mathilde Runge <maro@html24.net>
2019-12-02 15:41:04 +0100

    Required header.php and footer.php for singleProduct.php. Footer.php include a variable $sScriptPath, but maybe it's not needed if the script is always one big centralized script...

---
+++ singleProduct.php
@@ -0,0 +1,11 @@
+<?php 
+$sTitle = 'Proper Pour | Product name'; // Add the product dynamically
+require_once(__DIR__.'/components/header.php');
+?>
+
+
+<main></main>
+
+<?php
+$sScriptPath = 'script.js';
+require_once(__DIR__.'/components/footer.php');
