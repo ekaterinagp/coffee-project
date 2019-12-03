bee5a92d46ac3f6e080111f30442c64223532eb4
Mathilde Runge <maro@html24.net>
2019-12-03 14:47:22 +0100

    adjustment css and php for header, menu and footer, updated icons for menu

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,7 @@
     <footer class="grid grid-five bg-dark-brown ph-medium">
-      <div class="item logo bg-contain ph-small"></div>
+      <div class="item ph-small">
+      <?= file_get_contents(__DIR__.'/../img/the-proper-pour-logo.svg') ;?>
+      </div>
       <div class="item address color-white ph-small">
         <h5>Address</h5>
         <p>Lygten 37 2400 Copenhagen Denmark</p>
@@ -18,6 +20,5 @@
       </div>
     </footer>
     <script src="script.js"></script>
-    <script src="menu.js"></script>
   </body>
 </html>
