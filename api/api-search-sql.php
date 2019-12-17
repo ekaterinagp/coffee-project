8953c24ed80ff2770e9ad19c4faf00d518c85889
ekaterinagp <katya.koriakina@gmail.com>
2019-12-17 15:37:57 +0100

    api search prepared statement

--- api/api-search-sql.php
+++ api/api-search-sql.php
@@ -1,58 +1,27 @@
 <?php
-// require_once(__DIR__ . '/../connection.php');
-// if (empty($_GET['search']) && $_GET['search'] !== '0') {
-//   echo '[]';
-//   exit;
-// }
-// $sSearchInput = $_GET['search']; 
-
-
-
-// $query = "SELECT tproduct.cName AS productName, tproduct.nProductID,tproduct.nPrice, tcoffeetype.cName AS typeName, 
-// tcoffeetype.nCoffeeTypeID FROM  tproduct 
-// INNER JOIN tcoffeetype ON tproduct.nCoffeeTypeID=tcoffeetype.nCoffeeTypeID 
-// HAVING  productName LIKE  :search
-// OR  typeName LIKE  :search";
-
-// $statement = $connection->prepare($query);
-// $statement->bindValue(':search', "%{$sSearchInput}%");
-
-// echo json_encode($statement);
-// $result = $statement->fetchALL(PDO::FETCH_GROUP);
-// echo json_encode($result);
-// $statement->execute(array(':search' => $sSearchInput));
-// $statement->execute();
-
-// if ($statement->execute()) {
-//   $result = $statement->fetchALL(PDO::FETCH_GROUP);
-//   $arrayMatches = [];
-//   foreach ($result as $searchResult) {
-//     if ($searchResult !== false) {
-//       array_push($arrayMatches, $searchResult);
-//       $connection = null;
-//     }
-//   }
-//   echo json_encode($arrayMatches);
-//   exit;
-// } else {
-//   echo 0;
-//   $connection = null;
-//   exit;
-// }
-
 require_once(__DIR__ . '/../connection.php');
 if (empty($_GET['search']) && $_GET['search'] !== '0') {
   echo '[]';
   exit;
 }
 $sSearchInput = $_GET['search'];
+
+$data = [
+  ':search' => $sSearchInput,
+  ':search1' => $sSearchInput
+];
+
 $query = "SELECT tproduct.cName AS productName, tproduct.nProductID,tproduct.nPrice, tcoffeetype.cName AS typeName, 
 tcoffeetype.nCoffeeTypeID FROM  tproduct 
 INNER JOIN tcoffeetype ON tproduct.nCoffeeTypeID=tcoffeetype.nCoffeeTypeID 
-HAVING  productName LIKE '%" . $sSearchInput . "%'
-OR  typeName LIKE '%" . $sSearchInput . "%'";
+HAVING  productName LIKE CONCAT('%',:search,'%')
+OR  typeName LIKE CONCAT('%',:search1,'%')";
+
 $statement = $connection->prepare($query);
-if ($statement->execute()) {
+
+
+if ($statement->execute($data)) {
+
   $result = $statement->fetchALL();
   $arrayMatches = [];
   foreach ($result as $searchResult) {
@@ -63,7 +32,36 @@ if ($statement->execute()) {
   }
   echo json_encode($arrayMatches);
   exit;
+} else {
+  echo 0;
+  $connection = null;
+  exit;
 }
-echo 0;
-$connection = null;
-exit;
+
+// require_once(__DIR__ . '/../connection.php');
+// if (empty($_GET['search']) && $_GET['search'] !== '0') {
+//   echo '[]';
+//   exit;
+// }
+// $sSearchInput = $_GET['search'];
+// $query = "SELECT tproduct.cName AS productName, tproduct.nProductID,tproduct.nPrice, tcoffeetype.cName AS typeName, 
+// tcoffeetype.nCoffeeTypeID FROM  tproduct 
+// INNER JOIN tcoffeetype ON tproduct.nCoffeeTypeID=tcoffeetype.nCoffeeTypeID 
+// HAVING  productName LIKE '%" . $sSearchInput . "%'
+// OR  typeName LIKE '%" . $sSearchInput . "%'";
+// $statement = $connection->prepare($query);
+// if ($statement->execute()) {
+//   $result = $statement->fetchALL();
+//   $arrayMatches = [];
+//   foreach ($result as $searchResult) {
+//     if ($searchResult !== false) {
+//       array_push($arrayMatches, $searchResult);
+//       $connection = null;
+//     }
+//   }
+//   echo json_encode($arrayMatches);
+//   exit;
+// }
+// echo 0;
+// $connection = null;
+// exit;
