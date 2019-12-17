2e1736ede52c4912ea7b6a47a1cbd98802a04dd4
ekaterinagp <katya.koriakina@gmail.com>
2019-12-17 19:04:25 +0100

    deleted redundant code

--- api/api-search-sql.php
+++ api/api-search-sql.php
@@ -37,31 +37,3 @@ if ($statement->execute($data)) {
   $connection = null;
   exit;
 }
-
-// require_once(__DIR__ . '/../connection.php');
-// if (empty($_GET['search']) && $_GET['search'] !== '0') {
-//   echo '[]';
-//   exit;
-// }
-// $sSearchInput = $_GET['search'];
-// $query = "SELECT tproduct.cName AS productName, tproduct.nProductID,tproduct.nPrice, tcoffeetype.cName AS typeName, 
-// tcoffeetype.nCoffeeTypeID FROM  tproduct 
-// INNER JOIN tcoffeetype ON tproduct.nCoffeeTypeID=tcoffeetype.nCoffeeTypeID 
-// HAVING  productName LIKE '%" . $sSearchInput . "%'
-// OR  typeName LIKE '%" . $sSearchInput . "%'";
-// $statement = $connection->prepare($query);
-// if ($statement->execute()) {
-//   $result = $statement->fetchALL();
-//   $arrayMatches = [];
-//   foreach ($result as $searchResult) {
-//     if ($searchResult !== false) {
-//       array_push($arrayMatches, $searchResult);
-//       $connection = null;
-//     }
-//   }
-//   echo json_encode($arrayMatches);
-//   exit;
-// }
-// echo 0;
-// $connection = null;
-// exit;
